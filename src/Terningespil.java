c83748c6fb8a492070f07589894b9586cea633fa
Mikkel <mikkelsa97@gmail.com>
2020-09-23 14:44:02 +0200

    Spiller vælger

--- src/Terningespil.java
+++ src/Terningespil.java
@@ -1,6 +1,16 @@
 public class Terningespil {
 
+
+
     public static void main(String[] args) {
+
+        int min = 1;
+        int max = 3;
+
+
+        int Spiller = (int) (Math.random()*(max-min)+min);
+
+        System.out.println(Spiller);
 
     }
 
