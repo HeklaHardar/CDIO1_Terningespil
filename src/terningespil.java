433ff4225ad8e984071317c57b4f8ccfa9e3bd76
Lucas Knudsen <s195660@student.dtu.dk>
2020-09-23 13:33:24 +0200

    "Reset af master"

--- src/terningespil.java
+++ src/terningespil.java
@@ -1,20 +1,3 @@
 public class terningespil {
-    // here u go stupid
-    //your mom
-    //Hej
-    public static void main(String[] args) {
-        System.out.println("Er der forbindelse?");
-        System.out.println("Alle mine penge er blå");
-    }
-
 
 }
-
-
-
-// asdfghjklæøælkjhgfdsdfghjklælkjhgfd
-
-
-
-
-//skrrrt
