90931954ead887255f8ba671d924fb64e127e350
Mikkel <mikkelsa97@gmail.com>
2020-09-23 15:04:07 +0200

    "while loops for spiller 1 og spiller 2"

--- src/Spiller.java
+++ src/Spiller.java
@@ -1,2 +1,38 @@
 public class Spiller {
+
+    public static void main(String[] args) {
+
+        int min = 1;
+        int max = 3;
+        boolean spil = true;
+
+
+        int Spiller = (int) (Math.random() * (max - min) + min);
+
+        if (Spiller == 1) {
+            System.out.println("Spiller 1 starter");
+        } else {
+            System.out.println("Spiller 2 starter");
+        }
+
+       /* while(spil){
+
+            while(Spiller == 1) {
+
+
+            }
+
+            while(Spiller == 2){
+
+
+            }
+
+
+
+        }
+*/
+
+    }
+
+
 }
--- src/Terningespil.java
+++ src/Terningespil.java
@@ -1,24 +1,4 @@
 public class Terningespil {
 
 
-
-    public static void main(String[] args) {
-
-        int min = 1;
-        int max = 3;
-
-
-        int Spiller = (int) (Math.random()*(max-min)+min);
-
-        if (Spiller == 1) {
-            System.out.println("Spiller 1 starter");
-        } else {
-            System.out.println("Spiller 2 starter");
-        }
-
-
-    }
-
-
-
 }
