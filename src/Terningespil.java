64f3af5398a917040c2fb062b5c8f4d789e297df
Lucas Knudsen <s195660@student.dtu.dk>
2020-09-23 16:12:30 +0200

    Terningkast v3, kan nu bruges i andre classes

--- src/Terningespil.java
+++ src/Terningespil.java
@@ -1,8 +1,14 @@
 public class Terningespil {
 
     public static void main(String[] args) {
-        TerningKast.Kast();
+        // TerningKast.Kast();
 
+        TerningKast HentKommando = new TerningKast();
 
+        int sumafterninger = HentKommando.VaerdiTerning1() + HentKommando.VaerdiTerning2();
+
+        System.out.println(HentKommando.VaerdiTerning1() + " " + HentKommando.VaerdiTerning2());
+
+        System.out.println(sumafterninger);
     }
 }
