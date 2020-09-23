1e96b98d838c5695f039822f4b569f7a1dfb076b
Mikkel <mikkelsa97@gmail.com>
2020-09-23 13:14:42 +0200

    hej

--- src/terningespil.java
+++ src/terningespil.java
@@ -1,6 +1,7 @@
 public class terningespil {
     // here u go stupid
     //your mom
+    //hej
     public static void main(String[] args) {
         System.out.println("Er der forbindelse?");
     }
