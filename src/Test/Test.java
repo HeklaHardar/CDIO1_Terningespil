a22e36daa5eac4e5934499043a40192d03a61820
Mikkel <mikkelsa97@gmail.com>
2020-09-30 15:05:26 +0200

    "Test system "

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -5,5 +5,20 @@ import Spil.Terning;
 public class Test {
 
 
+    public static void main(String[] args) {
+
+        Raflebaeger raflebaeger = new Raflebaeger();
+
+
+        for(int i = 1000; i > 0; i--){
+
+           // raflebaeger
+
+
+
+        }
+
+
+    }
 
 }
