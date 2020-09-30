c90da39d1c360588008ed0a831bdaa201e9d4f08
WVN-AFK <54137049+WVN-AFK@users.noreply.github.com>
2020-09-30 15:59:14 +0200

    Version 0.0.1

--- src/terningespil.java
+++ src/terningespil.java
@@ -1,36 +1,49 @@
 import java.util.Scanner;
 
 public class terningespil {
-    // here u go stupid
-    //your mom
-
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
         int a = input.nextInt();
-
+        int ScoreA = 0;
+        int ScoreB = 0;
+        int b;
+        ScoreA = ScoreA + a;
         int n = 1;
-        while ( n <= a)
-        {
-            n = n + 1;
+        while (n <= ScoreA){
             System.out.print("█");
-
+            n++;
         }
+
+        /*do {
+            while (n <= a) {
+
+                System.out.print("█");
+                ScoreA = ScoreA + a;
+                n = n + 1;
+
+            }
             System.out.println(" ");
-        a = input.nextInt();
-        n = 1;
+            b = input.nextInt();
+            n = 1;
 
-        while ( n <= a)
-        {
-            n = n + 1;
-            System.out.print("█");
+            while (n <= b) {
 
-        }
-        System.out.println(" ");
+                System.out.print("█");
 
-        }
+                n = n + 1;
+            }
+            ScoreB = ScoreB + b;
+
+
+            System.out.print(" " + ScoreB);
+
+        }*/
 
     }
+}
+
 
 
 
