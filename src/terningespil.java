085200bc2924727bd7792a3e17ff3ef4691776ae
WVN-AFK <54137049+WVN-AFK@users.noreply.github.com>
2020-09-30 14:44:30 +0200

    Tester en graf format

--- src/terningespil.java
+++ src/terningespil.java
@@ -3,16 +3,27 @@ import java.util.Scanner;
 public class terningespil {
     // here u go stupid
     //your mom
+
     public static void main(String[] args) {
+
+        int n = 0;
+
         Scanner input = new Scanner(System.in);
         System.out.println("Er der forbindelse?");
         System.out.println("Alle mine penge er blå");
-        int a = nextInt();
+        int a = input.nextInt();
         System.out.println(a);
+        if (a => n) {
+            n++;
+            System.out.println("█");
+        }
+
+        }
     }
 
 
-}
+
+
 
 
 
