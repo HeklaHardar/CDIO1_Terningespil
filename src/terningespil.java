6d283716b82d6494a6216e7d93856b1617ff6505
WVN-AFK <54137049+WVN-AFK@users.noreply.github.com>
2020-10-01 14:35:47 +0200

    Version 0.3.0

--- src/terningespil.java
+++ src/terningespil.java
@@ -11,14 +11,19 @@ public class terningespil {
         int sum = 0;
         int n = 0;
         int combo = 0;
-        int iteration = 100;
+        int iteration = 1000;
         double c1 = 0;
         double c2 = 0;
         double c3 = 0;
         double c4 = 0;
         double c5 = 0;
         double c6 = 0;
-
+        double c7 = 0;
+        double c8 = 0;
+        double c9 = 0;
+        double c10 = 0;
+        double c11 = 0;
+        double c12 = 0;
 
         while (n <= iteration) {
             terning1 = (int) (Math.random() * (7 - 1) + 1);
@@ -32,31 +37,42 @@ public class terningespil {
             }
 
             if (sum == 2){
-                c1 = c1 + 1;
-            } else if (sum == 4){
                 c2 = c2 + 1;
-            } else if (sum == 6){
-                c3 = c3 + 3;
-            } else if (sum == 8){
+            } else if (sum == 3){
+                c3 = c3 + 1;
+            } else if (sum == 4){
                 c4 = c4 + 1;
-            } else if (sum == 10){
+            } else if (sum == 5){
                 c5 = c5 + 1;
-            } else if (sum == 12) {
+            } else if (sum == 6){
                 c6 = c6 + 1;
+            } else if (sum == 7) {
+                c7 = c7 + 1;
+            } else if (sum == 8){
+                c8 = c8 + 1;
+            } else if (sum == 9){
+                c9 = c9 + 1;
+            } else if (sum == 10){
+                c10 = c10 + 1;
+            } else if (sum == 11){
+                c11 = c11 + 1;
+            } else if (sum == 12){
+                c12 = c12 + 1;
             }
+
 
 
         }
         System.out.println("-----------------------------");
-        System.out.println("n = " + n + "        " + terning1 + "        " + terning2);
+        System.out.println("n = " + (n - 1) + "        " + terning1 + "        " + terning2);
         System.out.println("Sum er: " + sum);
         System.out.println("-----------------------------");
 
-        int i = 1;
+
         System.out.println("2x comboer: " + combo);
         System.out.println(" ");
 
-
+        int i = 1;
         n = 1;
         if (iteration <= 100) {
             i = 1;
@@ -67,48 +83,97 @@ public class terningespil {
         }
 
 
-        while (n <= c1/i) {
-            System.out.print("█");
-            n++;
-        }
-        n = 1;
-        System.out.print(" sum = 2 | " + c1/i + " * "+ i );
-        System.out.println(" ");
         while (n <= c2/i) {
             System.out.print("█");
             n++;
         }
         n = 1;
-        System.out.print(" sum = 4 | " + c2/i + " * "+ i);
+        System.out.print(" sum = 2 | " + c2/i + " * "+ i );
         System.out.println(" ");
+
         while (n <= c3/i) {
             System.out.print("█");
             n++;
         }
         n = 1;
-        System.out.print(" sum = 6 | " + c3/i + " * "+ i);
+        System.out.print(" sum = 3 | " + c3/i + " * "+ i);
         System.out.println(" ");
+
         while (n <= c4/i) {
             System.out.print("█");
             n++;
         }
         n = 1;
-        System.out.print(" sum = 8 | " + c4/i + " * "+ i);
+        System.out.print(" sum = 4 | " + c4/i + " * "+ i);
         System.out.println(" ");
+
         while (n <= c5/i) {
             System.out.print("█");
             n++;
         }
         n = 1;
-        System.out.print(" sum = 10 | " + c5/i + " * "+ i);
+        System.out.print(" sum = 5 | " + c5/i + " * "+ i);
         System.out.println(" ");
+
         while (n <= c6/i) {
             System.out.print("█");
             n++;
         }
-        System.out.print(" sum = 12 | " + c6/i + " * "+ i);
+        n = 1;
+        System.out.print(" sum = 6 | " + c6/i + " * "+ i);
+        System.out.println(" ");
+
+        while (n <= c7/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 7 | " + c7/i + " * "+ i);
+        System.out.println(" ");
+
+        while (n <= c8/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 8 | " + c8/i + " * "+ i);
+        System.out.println(" ");
+
+        while (n <= c9/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 9 | " + c9/i + " * "+ i);
+        System.out.println(" ");
+
+        while (n <= c10/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 10 | " + c10/i + " * "+ i);
+        System.out.println(" ");
+
+        while (n <= c11/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 11 | " + c11/i + " * "+ i);
+        System.out.println(" ");
+        while (n <= c12/i) {
+            System.out.print("█");
+            n++;
+        }
+        n = 1;
+        System.out.print(" sum = 12 | " + c12/i + " * "+ i);
+
+
 
         }
+
+
     }
 
 
