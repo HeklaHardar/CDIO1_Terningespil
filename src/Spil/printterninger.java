cfe0cc64e854f181290433b3e037d9671938d5d1
Hekla <hekla@kuzin.dk>
2020-10-01 13:32:36 +0200

    Push print fil

---
+++ src/Spil/printterninger.java
@@ -0,0 +1,11 @@
+package Spil;
+
+public class printterninger {
+    static int findTerning(int vaerdi){
+
+        System.out.println("yep");
+        vaerdi=666;
+        return vaerdi;
+    }
+
+}
